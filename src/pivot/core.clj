bddc1e05bf63ae2428ee17baff62bd75823ca582
Valeriy Manenkov <v.manenkov@gmail.com>
2017-09-03 23:10:09 +0300

    Initial import

--- src/pivot/core.clj
+++ src/pivot/core.clj
@@ -1,6 +1,48 @@
 (ns pivot.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn- apply-fns
+  [fns coll]
+  (->> fns
+       (map #(% coll))))
+    
+(defn- apply-filters
+  [filters coll]
+  (loop [filters filters
+         coll coll]
+       (if (empty? filters)
+           coll
+           (recur (rest filters)
+                  (filter (first filters) coll)
+                  ))))
+
+(defn- map-vals
+  [m f]
+  (zipmap (keys m) (map f (vals m))))
+
+(defn- nested-group-by
+  [fs coll & [final-fn]]
+  (if (empty? fs)
+    ((or (partial apply-fns final-fn) identity) coll)
+    (map-vals (group-by (first fs) coll)
+                              #(nested-group-by (rest fs) % final-fn))))
+
+(defn- flatten-keys [m]
+  (if (not (map? m))
+    {[] m}
+    (into {}
+          (for [[k v] m
+                [ks v'] (flatten-keys v)]
+            [(cons k ks) v']
+          ))))
+
+(defn flatten-data [m ks]
+  (->> (seq (flatten-keys m))
+       (map #(concat (first %) (second %)))
+       (map (partial zipmap ks))
+       ))
+
+(defn pivot [filters fs coll final-fn]
+  (flatten-data (nested-group-by fs
+                   (apply-filters filters coll)
+                   (vals final-fn)) (concat fs (keys final-fn))
+		   ))
--- test/pivot/core_test.clj
+++ test/pivot/core_test.clj
@@ -2,6 +2,43 @@
   (:require [clojure.test :refer :all]
             [pivot.core :refer :all]))
 
+(def data
+  [{:id 1 :product "Carrots" :category "Vegetables" :amount 4270 :date "1-6-2016" :country "United States"}
+   {:id 2 :product "Broccoli" :category "Vegetables" :amount 8239 :date "1-7-2016" :country "United Kingdom"}
+   {:id 3 :product "Banana" :category "Fruit" :amount 617 :date "1-8-2016" :country "United States"}
+   {:id 4 :product "Banana" :category "Fruit" :amount 8384 :date "1-10-2016" :country "Canada"}
+   {:id 5 :product "Beans" :category "Vegetables" :amount 2626 :date "1-10-2016" :country "Germany"}
+   {:id 6 :product "Orange" :category "Fruit" :amount 3610 :date "1-11-2016" :country "United States"}
+   {:id 7 :product "Broccoli" :category "Vegetables" :amount 9062 :date "1-11-2016" :country "Australia"}
+   {:id 8 :product "Banana" :category "Fruit" :amount 6906 :date "1-16-2016" :country "New Zealand"}
+   {:id 9 :product "Apple" :category "Fruit" :amount 2417 :date "1-16-2016" :country "France"}
+   {:id 10 :product "Apple" :category "Fruit" :amount 7421 :date "1-16-2016" :country "Canada"}])
+
 (deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+  (testing "Test 1"
+    (is (= (pivot [] [:category] data {:count count})
+    	   [{:category "Vegetables", :count 4} {:category "Fruit", :count 6}]))
+    ))
+
+(deftest b-test
+  (testing "Test 2"
+    (is (= (pivot [] [:category] data {:amount (fn [x] (->> x (map :amount) (reduce +)))})
+    	   [{:category "Vegetables", :amount 24197} {:category "Fruit", :amount 29355}]))
+    ))
+
+(deftest c-test
+  (testing "Test 3"
+    (is (= (pivot [(fn [x] (> (get x :amount) 1000))]
+                  [:category :date]
+                  data
+                  {:count count :amount (fn [x] (->> x (map :amount) (reduce +)))})
+    	   [{:category "Vegetables", :date "1-6-2016", :count 1, :amount 4270}
+	    {:category "Vegetables", :date "1-7-2016", :count 1, :amount 8239}
+            {:category "Vegetables", :date "1-10-2016", :count 1, :amount 2626}
+            {:category "Vegetables", :date "1-11-2016", :count 1, :amount 9062}
+            {:category "Fruit", :date "1-10-2016", :count 1, :amount 8384}
+            {:category "Fruit", :date "1-11-2016", :count 1, :amount 3610}
+            {:category "Fruit", :date "1-16-2016", :count 3, :amount 16744}]))
+    ))
+
+
