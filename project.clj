f9419082cc873b39215709d3eb7fb62df0b21358
Valeriy Manenkov <v.manenkov@gmail.com>
2018-07-15 20:53:40 +0300

    Disable signing on release

--- project.clj
+++ project.clj
@@ -4,4 +4,12 @@
   :url "https://github.com/char16t/pivot"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :release-tasks [["vcs" "assert-committed"]
+                  ["change" "version" "leiningen.release/bump-version" "release"]
+                  ["vcs" "commit"]
+                  ["vcs" "tag" "--no-sign"]
+                  ["deploy"]
+                  ["change" "version" "leiningen.release/bump-version"]
+                  ["vcs" "commit"]
+                  ["vcs" "push"]])
